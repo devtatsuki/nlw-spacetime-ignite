d76bc300ba5edf5b2ea92f22292fb3fe72067fc9
Tatsuki <zyramvs@gmail.com>
2023-05-20 14:27:19 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,42 @@
+import 'dotenv/config'
+
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import fstatic from '@fastify/static'
+
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
+
+const app = fastify()
+
+app.register(multipart)
+
+app.register(fstatic, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+
+app.register(cors, {
+  origin: true,
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 Server running on port 3333')
+  })
---
+++ web/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['avatars.githubusercontent.com', '192.168.3.110'],
+  },
+}
+
+module.exports = nextConfig
